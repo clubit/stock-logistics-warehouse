16686badc6dfe1cfb78991d4344a3a3605c8e410
Odoo <odoo@clubit.be>
2016-09-28 17:50:21 +0000

    ADD: stock blanket orders fields / views

---
+++ stock_blanket_orders/README.rst
@@ -0,0 +1,45 @@
+Features
+========
+
+This module was written to enter allow efficient tracking of blanket orders in 2-step receptions. 
+
+Screenshots
+===========
+
+Installation
+============
+
+To install this module, you just need to select the module and insure yourself dependencies are available
+
+Configuration
+=============
+
+No particular configuration to use this module
+
+Usage
+=====
+
+To use this module, you need to :
+
+- create a picking and enter its properties
+
+Known issues / Roadmap
+======================
+
+* n/a
+
+Contributors
+============
+
+* Dimitri Verhelst <dimitriverhelst@clubit.be>
+
+Credits
+=======
+
+* Module developed and tested with Odoo version 8.0
+* For questions, please contact our support services <informatie@clubit.be>
+
+Maintainer
+==========
+
+This module is maintained by Clubit BVBA.
---
+++ stock_blanket_orders/__openerp__.py
@@ -0,0 +1,39 @@
+# -*- encoding: utf-8 -*-
+##############################################################################
+#
+#    Odoo, Open Source Management Solution
+#    This module copyright (C) 2016 Clubit BVBA
+#    (<https://clubit.be>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+{
+    'name': 'Stock Blanket Orders',
+    'version': '0.1',
+    'author': 'Clubit BVBA',
+    'maintainer': 'Clubit BVBA',
+    'website': 'https://clubit.be',
+    'license': 'AGPL-3',
+    'category': 'Warehouse',
+    'summary': "Extend picking view with fields that allow blanket orders to run efficiently",
+    'depends': [
+        'stock',
+    ],
+    'data': [
+        'views/stock_picking.xml'
+    ],
+    'installable': True,
+    'application': False,
+}
